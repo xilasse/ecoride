3bb2808e25b230dc937027951c73e2474a855f8e
ATM|LaciT <lordikkop@hotmail.fr>
2025-09-24 19:19:13 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '../config/database.php';
+require_once __DIR__ . '/../config/database.php';
 
 // Charger les variables d'environnement
 if (file_exists(__DIR__ . '/../.env')) {
